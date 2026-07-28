a18237d9e4920857516e8ca1d57d418ecc70e70e
souleymane-b <devsouleymane97@gmail.com>
2026-07-28 14:00:33 +0000

    Updated Team.tsx

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -46,7 +46,7 @@ const team: Member[] = [
     bioEn: "Passionate and meticulous developer, Souleymane turns ideas into functional applications. From front-end to back-end, he codes with precision and ensures performance.",
     expertise: ['React', 'Node.js', 'APIs'],
     expertiseEn: ['React', 'Node.js', 'APIs'],
-    photo: 'https://chatgpt.com/backend-api/estuary/content?id=file_0000000079c881f4970f7e053b8374b6&ts=495901&p=fs&cid=1&sig=67355c5a6c0e7ce90329894f0b373a9b03288809dc4377d9c57e428517b77d2a&v=0',
+    photo: 'https://cdn.discordapp.com/attachments/1348907267956670474/1531661627630420109/content.png?ex=6a6a068c&is=6a68b50c&hm=9f2ad8fb3ca549dff1224c287799ece3a414bed0b530f0b86ec842787bdc1e66&',
     socials: { linkedin: '#', twitter: '#', email: 'souleymane@tellyintech.com' },
     accent: 'orange',
   },
